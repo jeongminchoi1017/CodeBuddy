f6ebe58af8e91c24626168ec2a264aa3d5718807
jeongminchoi1017 <jeongminchoi1017@gmail.com>
2023-12-15 17:46:15 +0900

    study

---
+++ src/main/frontend/src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>header입니다.</h1>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    text-decoration: underline;
+  }
+</style>
---
+++ src/main/frontend/src/components/Main.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h3>Main 화면입니다.</h3>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    margin:50px;
+  }
+</style>
